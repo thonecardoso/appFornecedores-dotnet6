f87e435c78f0539da5935bcdacf6d9c0d7163544
Thone <46010284+thonecardoso@users.noreply.github.com>
2023-01-25 22:59:38 -0300

    Improve provider views

---
+++ AppFornecedores/src/DevIO.Application/Extensions/RazorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace DevIO.Application.Extensions;
+
+public static class RazorExtensions
+{
+    public static string FormatDocument(this RazorPage page, int personType, string documento)
+    {
+        return personType == 1 ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00") : Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+    }
+
+}
